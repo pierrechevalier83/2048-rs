28af96654e3250caa26cf503808133295498b811
Pierre Chevalier <pierrechevalier83@gmail.com>
2017-02-28 01:48:43 +0000

    Start implementing game logic
    Add Empty cell representation, initialize grid with 2 randomly
    positioned cells containing "2"

--- src/board.rs
+++ src/board.rs
@@ -3,28 +3,31 @@ use self::matrix_display::*;
 
 pub struct Board {
     format: Format,
-    colour_theme: [i32; 16],
+    colour_theme: [i32; 17],
 }
 
 impl Board {
     pub fn new() -> Board {
-        let colour_theme = [247, 78, 222, 220, 214, 208, 202, 196, 162, 160, 126, 90, 88, 54, 53,
-                            52];
+        let colour_theme = [0, 247, 78, 222, 220, 214, 208, 202, 196, 162, 160, 126, 90, 88, 54,
+                            53, 52];
 
         Board {
             format: Format::new(7, 3),
             colour_theme: colour_theme,
         }
     }
-    fn cells(&self, data: ::std::ops::Range<i32>) -> Vec<cell::Cell<i32>> {
-        data.map(|x| {
-                cell::Cell::new(2_f64.powi(x + 1) as i32,
-                                7,
-                                *self.colour_theme.get(x as usize).unwrap() as u8)
+    fn cells(&self, data: [i32; 16]) -> Vec<cell::Cell<String>> {
+        data.iter()
+            .cloned()
+            .map(|i| (2_f64.powi(i), *self.colour_theme.get(i as usize).unwrap() as u8))
+            .map(|(x, col)| match x {
+                1_f64 => (".".to_string(), col),
+                _ => (x.to_string(), col),
             })
+            .map(|(s, col)| cell::Cell::new(s, 7, col))
             .collect::<Vec<_>>()
     }
-    pub fn print(self, data: ::std::ops::Range<i32>) {
+    pub fn print(self, data: [i32; 16]) {
         let matrix = matrix::Matrix::new(4, self.cells(data));
         let display = MatrixDisplay::new(self.format, matrix);
         display.print(&mut ::std::io::stdout(), &style::BordersStyle::Thick);
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,48 @@
+extern crate rand;
+
 mod board;
+use rand::{thread_rng, Rng};
+
+enum Move {
+    up,
+    down,
+    left,
+    right,
+}
+
+enum GameStatus {
+    ongoing,
+    won,
+    lost,
+    interrupted,
+}
+
+struct Game {
+    status: GameStatus,
+    score: i32,
+    data: [i32; 16],
+}
+
+impl Game {
+    pub fn new() -> Game {
+        let mut rng = thread_rng();
+        let mut data = [0; 16];
+        data[0] = 1;
+        data[1] = 1;
+        rng.shuffle(&mut data);
+        Game {
+            status: GameStatus::ongoing,
+            score: 0,
+            data: data,
+        }
+    }
+    fn data(self) -> [i32; 16] {
+        self.data
+    }
+}
 
 fn main() {
     let board = board::Board::new();
-    board.print((0..16));
+    let game = Game::new();
+    board.print(game.data());
 }
