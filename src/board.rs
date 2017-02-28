9ddd977d7a0734d614ac953ba1954a28f03de3cb
Pierre Chevalier <pierrechevalier83@gmail.com>
2017-02-28 00:18:04 +0000

    Board: only get the data at print time

--- src/board.rs
+++ src/board.rs
@@ -3,27 +3,30 @@ use self::matrix_display::*;
 
 pub struct Board {
     format: Format,
-    matrix: matrix::Matrix<cell::Cell<i32>>,
+    colour_theme: [i32; 16],
 }
 
 impl Board {
     pub fn new() -> Board {
-        let colour_theme = vec![247, 78, 222, 220, 214, 208, 202, 196, 162, 160, 126, 90, 88, 54,
-                                53, 52];
-        let board = (0..16)
-            .map(|x| {
-                cell::Cell::new(2_f64.powi(x + 1) as i32,
-                                7,
-                                *colour_theme.get(x as usize).unwrap() as u8)
-            })
-            .collect::<Vec<_>>();
+        let colour_theme = [247, 78, 222, 220, 214, 208, 202, 196, 162, 160, 126, 90, 88, 54, 53,
+                            52];
+
         Board {
             format: Format::new(7, 3),
-            matrix: matrix::Matrix::new(4, board),
+            colour_theme: colour_theme,
         }
     }
-    pub fn print(self) {
-        let display = MatrixDisplay::new(self.format, self.matrix);
+    fn cells(&self, data: ::std::ops::Range<i32>) -> Vec<cell::Cell<i32>> {
+        data.map(|x| {
+                cell::Cell::new(2_f64.powi(x + 1) as i32,
+                                7,
+                                *self.colour_theme.get(x as usize).unwrap() as u8)
+            })
+            .collect::<Vec<_>>()
+    }
+    pub fn print(self, data: ::std::ops::Range<i32>) {
+        let matrix = matrix::Matrix::new(4, self.cells(data));
+        let display = MatrixDisplay::new(self.format, matrix);
         display.print(&mut ::std::io::stdout(), &style::BordersStyle::Thick);
     }
 }
