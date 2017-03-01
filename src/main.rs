854f7bd984c72652098131b60412391160983ffc
Pierre Chevalier <pierrechevalier83@gmail.com>
2017-03-01 01:52:58 +0000

    Add unit tests for slide_left

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,24 @@ mod slide_right_test {
         assert_eq!(vec![0, 0, 1, 2], slide_right([1, 0, 2, 0]));
     }
     #[test]
+    fn test_slide_left_with_two_different_elements() {
+        assert_eq!(vec![1, 2, 0, 0], slide_left([1, 0, 2, 0]));
+    }
+    #[test]
     fn test_slide_right_with_two_same_elements() {
         assert_eq!(vec![0, 0, 0, 2], slide_right([1, 0, 1, 0]));
     }
     #[test]
+    fn test_slide_left_with_two_same_elements() {
+        assert_eq!(vec![2, 0, 0, 0], slide_left([1, 0, 1, 0]));
+    }
+    #[test]
     fn test_slide_right_with_three_same_elements() {
         assert_eq!(vec![0, 0, 1, 2], slide_right([1, 0, 1, 1]));
+    }
+    #[test]
+    fn test_slide_left_with_three_same_elements() {
+        assert_eq!(vec![2, 1, 0, 0], slide_left([1, 0, 1, 1]));
     }
     #[test]
     fn test_slide_right_with_three_different_elements() {
@@ -34,12 +46,26 @@ mod slide_right_test {
         assert_eq!(vec![0, 0, 2, 2], slide_right([0, 1, 1, 2]));
     }
     #[test]
+    fn test_slide_left_with_three_different_elements() {
+        assert_eq!(vec![2, 2, 0, 0], slide_left([1, 0, 1, 2]));
+        assert_eq!(vec![2, 1, 2, 0], slide_left([2, 0, 1, 2]));
+        assert_eq!(vec![2, 2, 0, 0], slide_left([0, 1, 1, 2]));
+    }
+    #[test]
     fn test_slide_right_with_four_same_elements() {
         assert_eq!(vec![0, 0, 2, 2], slide_right([1, 1, 1, 1]));
     }
     #[test]
+    fn test_slide_left_with_four_same_elements() {
+        assert_eq!(vec![2, 2, 0, 0], slide_left([1, 1, 1, 1]));
+    }
+    #[test]
     fn test_slide_right_with_four_different_elements() {
         assert_eq!(vec![1, 2, 1, 2], slide_right([1, 2, 1, 2]));
+    }
+    #[test]
+    fn test_slide_left_with_four_different_elements() {
+        assert_eq!(vec![1, 2, 1, 2], slide_left([1, 2, 1, 2]));
     }
 }
 
